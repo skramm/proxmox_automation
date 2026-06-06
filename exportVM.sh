a2260e2d8eb0d5b398d991ac63066930abceebec
skramm <sebastien.kramm@univ-rouen.fr>
2026-06-06 20:09:30 +0200

    wip

--- exportVM.sh
+++ exportVM.sh
@@ -1,17 +1,25 @@
 
 # Génération d'une liste de VM
+
 set -x
+
+# désactive l'interprétation de '!' dans une chaine
+set +H
+
 # arg
 
 USER=$1
 NB=$2
 
+TODAY=$(date '+%Y%m%d_%H%M%S')
 out=out.csv
+
+CURLLOG=curl_$TODAY.log
 
 function process()
 {
 	curl -k -H 'Authorization: PVEAPIToken=krammseb@UR!'"${TOK_NAME}=${TOK_VALUE}" \
-		https://anvers.univ-rouen.fr:8006/api2/json/nodes/$1/qemu>>out_$1.json 2>>curl_log.log
+		https://anvers.univ-rouen.fr:8006/api2/json/nodes/$1/qemu>>out_$1.json 2>>$CURLLOG
 		
 #		 | jq -r '.data[] | [.vmid|tostring,.name] | join (";")' >> out_$1.csv 
 	err=$?
@@ -21,30 +29,33 @@ function process()
 	fi
 }
 
+# read token details
 source ../token_20260110
 
-echo "TOK_NAME
-# test de la réponse de l'API
+#echo "TOK_NAME"
+# "test de la réponse de l'API
 
 response=$(curl -s -k \
-  -H 'Authorization: PVEAPIToken=$USER@UR!'"${TOK_NAME}=${TOK_VALUE}" \
+  -H "Authorization: PVEAPIToken=$USER@UR!${TOK_NAME}=${TOK_VALUE}" \
   https://anvers.univ-rouen.fr:8006/api2/json/version)
 
 echo "Réponse API=$response"
 
+
 if [ $? -eq 0 ] && [ -n "$response" ]; then
     echo "API disponible"
 else
-    echo "API indisponible"
+    echo "API indisponible, réponse='$response'"
 	exit 1
 fi
-
+exit
 
 
 # liste des nodes 
 	curl -k -H "Authorization: PVEAPIToken=krammseb@UR!${TOK_NAME}=${TOK_VALUE}" \
-		https://anvers.univ-rouen.fr:8006/api2/json/nodes|jq | jq -r '.data[] | .node' >nodes.csv 2>>curl_log.log
+		https://anvers.univ-rouen.fr:8006/api2/json/nodes|jq | jq -r '.data[] | .node' >nodes.csv 2>>$CURLLOG
 
+exit
 while read -r nodename
 do
 	echo "process node $nodename"
--- genVM.sh
+++ genVM.sh
@@ -1,25 +1,75 @@
 
 # Script de gestion d'un ensemble de machines
 
+# !!!!!!!!!!!! WIP !!!!!!!!!!!!
+
 # arg
 
-=$1
-NB=$2
-
-echo "Donner le nom du token"
-read TOKEN_NAME
-
-echo "Copie la clé"
-read TOKEN_SECRET
+# désactive l'interprétation de '!' dans une chaine
+set +H
 
 
 
+# read token details
+source ../token_20260110
+
+
+NODE=$(zenity --list --text="Choisissez le noeud" --column="nodes" anvers gand bruxelles)
+if [ $? != 0 ]; then
+	echo "Interruption"; exit 1
+fi
+
+echo "NODE=$NODE"
+
+# Afficher la liste des templates dispos sur ce noeud
+
+# TODO faire une requete sur le noeud pour lister les machines et en extraire celles qui sont des template
+
+TEMPLATE=$(zenity --list --text="Quel template à utiliser?" --column="templates" $TEMPLATES)
+if [ $? != 0 ]; then
+	echo "Interruption"; exit 1
+fi
+echo "TEMPLATE=$TEMPLATE"
+
+
+NB=$(zenity --scale --text "Combien de VM" --step=1 \
+	--value=1 \
+	--min-value=1 \
+	--max-value=30)
+if [ $? != 0 ]; then
+	echo "Interruption"; exit 1
+fi
+
+echo "NB=$NB"
+
+
+MOD=$(zenity --entry --text "Donner le nom du module (RXXX, SAETTT)")
+if [ $? != 0 ]; then
+	echo "Interruption"; exit 1
+fi
+echo "MOD=$MOD"
+
+
+TAGS=$(zenity --entry --text "Donner les tags a associer aux VMs")
+if [ $? != 0 ]; then
+	echo "Interruption"; exit 1
+fi
+echo "TAGS=$TAGS"
+
+
+START=$(zenity --question --text "Ok pour créer $NB VM, nommées ${MOD}_1 à ${MOD}_NB, basée sur le template $TEMPLATE et ayant les tags $TAGS?")
+if [ $? != 0 ]; then
+	echo "Interruption"; exit 1
+fi
+echo "START=$START"
+
+exit
 
 for a in 
 do
 
 curl -k \
-	-H 'Authorization: PVEAPIToken=${USER}@UR!${TOKEN_NAME}=${TOKEN_SECRET}' \
+	-H "Authorization: PVEAPIToken=${USER}@UR!${TOK_NAME}=${TOK_VALUE}" \
 	https://anvers.univ-rouen.fr:8006/api2/json/
 
 done
